2b338fee88e3a8c9463d1ec37952426be90b9bfa
umesha-hb <umesha.kit@gmail.com>
2023-05-16 23:43:49 +0530

    first commit

---
+++ src/main/java/com/uttara/aws/lambda/OrderHandler.java
@@ -0,0 +1,6 @@
+package com.uttara.aws.lambda;
+
+import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
+
+public class OrderHandler extends SpringBootRequestHandler<String, Object> {
+}
